8316833d911cf2047cfff33c1353fe9dc8d59d02
Danijela2019 <arsic_danijela@yahoo.com>
2021-01-27 13:56:44 +0100

    Implemented JWT

--- public/index.js
+++ public/index.js
@@ -19,3 +19,30 @@ const clearForm = (event) => {
     event.preventDefault();
     document.getElementById("login-form").reset()
 };
+//////////////////////////////////////////////////////////////////////////
+const clearForm1 = (event) => {
+    event.preventDefault();
+    document.getElementById("login-form1").reset()
+};
+
+const loginUser = async (event) => {
+    event.preventDefault();
+    const userName= document.getElementById("login-input-name1").value;
+    const password = document.getElementById("login-input-password1").value;
+    const options = {
+        method: 'POST',
+        headers:{
+        'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({userName, password})
+    }
+    const resault = await fetch('/api/login',options)
+    .then((res)=> res.json())
+    if(resault.status === "ok"){
+        console.log('The token is', resault.data)
+
+    }else {
+        console.log('Resault.error', resault.error)
+    }
+}
+
