d9332dc7b9c6c15537dc4a933b5cb61ffcf705a3
Danijela2019 <arsic_danijela@yahoo.com>
2021-01-29 19:20:19 +0100

    fix: refactor hard coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const routes = require('./routes');
 
-mongoose.connect('mongodb://localhost:27017/authentication', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+const userName = process.env.MONGO_USERNAME;
+//const pass = process.env.MONGO_PASSWORD;
+//const port = process.env.PORT || 5000;
+//const CONNECTION_URL = `mongodb+srv://${userName}:${pass}@user-authentication.sjapq.mongodb.net/authentication?retryWrites=true&w=majority`;
+const CONNECTION_URL = process.env.MONGODB_URI;
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() =>
+    server.listen(port, () => {
+      console.log(`Server listening at ${port}`);
+    }),
+  )
+  .catch((err) => console.log(err.message));
 
 server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static('public'));
 server.use('/', routes);
 
@@ -27,10 +40,4 @@ server.use((error, _req, res, _next) => {
       message: error.message,
     },
   });
-});
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log(`Server listening at ${port}`);
 });
